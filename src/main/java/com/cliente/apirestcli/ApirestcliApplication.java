89e686cc330c429e3807bf9cbd0f12032688b5b6
hvalmer <hvalmer@gmail.com>
2021-05-03 11:03:29 -0300

    Project created

---
+++ src/main/java/com/cliente/apirestcli/ApirestcliApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.apirestcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestcliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cliente/apirestcli/ApirestcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.apirestcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
